0550ce9b57cb11b0874f5eca8f7761cfa6880379
byungil-park <bbi05060708@gmail.com>
2023-06-11 16:09:14 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='Ssub', version='0.7',
+      py_modules=['termProject', 'graph', 'LineMap', 'RealTimeDv', 'teller2', 'noti'],
+      package_data=['LineMap.png']
+      )
